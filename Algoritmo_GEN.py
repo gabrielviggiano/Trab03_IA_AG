0011c520e37c493700bf5bf0b70a4b5ff401c987
gmarchezi <gabriel_marchezi@hotmail.com>
2019-10-25 00:56:33 -0300

    Create Algoritmo_GEN.py
    Código em python do algoritmo genético.

---
+++ Algoritmo_GEN.py
@@ -0,0 +1,99 @@
+import random
+import math
+
+#Inicializa a população com valores aleatórios
+def InicializarPopulação (numPop):
+	listaPop = []
+	individuo = 0
+	while (numPop > 0):
+		individuo = random.randint(0,1024)
+		listaPop.append(DecToBin(individuo))
+		numPop = numPop - 1
+	return listaPop
+
+
+def Torneio(populacao):
+	lista_torneio = []
+	for i in range(len(populacao)):
+		pai1 = populacao[random.randint(0,len(populacao)-1)]
+		pai2 = populacao[random.randint(0,len(populacao)-1)]
+		while (pai1 == pai2):
+			pai2 = populacao[random.randint(0, len(populacao)-1)]
+		pt_pai1 = Avalia_Individuo(pai1)
+		pt_pai2 = Avalia_Individuo(pai2)
+		if pt_pai1 < pt_pai2:
+			lista_torneio.append(pai1)
+		else:
+			lista_torneio.append(pai2)
+	return lista_torneio
+
+
+
+def Melhor_Pior (populacao):
+	lista = []
+	melhor = populacao[0]
+	pior = populacao[0]
+	for individuo in populacao:
+		pt_individuo = Avalia_Individuo(individuo)
+		if (pt_individuo < Avalia_Individuo(melhor)):
+			melhor = individuo
+		if(pt_individuo > Avalia_Individuo(pior)):
+			pior = individuo
+	lista.append(melhor)
+	lista.append(pior)
+	return lista
+
+
+def Algo_Gen(iteracoes ,populacao):
+	lista = []
+	contador = 0
+	otimo = 0
+	while(cont<iteracoes and otimo == 0):
+		
+		cont += cont
+	return 0
+
+# Converte um número decimal para um número binário de 10 bits
+def DecToBin(dec):
+	b1n = bin(dec)[2:]
+	while (len(b1n) < 10):
+		b1n = '0' + b1n
+	return b1n
+
+# Convente um número binário para um número decimal
+def BinToDec(b1n):
+	i = 0
+	while (b1n[i] =='0'):
+		i += 1
+	b1n = '0b' + b1n[i:]
+	dec = int(b1n,2)
+	return dec
+
+
+# Avalia a aptidão do invidíduo
+def Avalia_Individuo(individuo):
+	x = (40/1024)*BinToDec(individuo)-20
+	resultado = round(math.cos(x)*x+2,2)
+	return resultado
+
+
+def main():
+	populacao = []
+	populacao = InicializarPopulação(10)
+	print(populacao)
+	#Algo_Gen(populacao)
+	lista = []
+	lista = Torneio(populacao)
+	print(lista)
+	print("--------------------------------------------------------------------------")
+	lista2 = Melhor_Pior(populacao)
+	print(lista2)	
+	print("--------------------------------------------------------------------------")
+	lista3 = Melhor_Pior(lista)
+	print(lista3)
+
+
+
+
+if __name__ == '__main__':
+	main()
